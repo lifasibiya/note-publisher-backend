0ab341a0a7c633649c8a4bb3fb22adb5476f3942
lifasibiya <sibiyalifa@gmail.com>
2025-03-09 16:14:08 +0200

    get notes done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('api/notes', (req, res) => {
-    res.json({message: 'Success'});
+app.get('/api/notes', async (req, res) => {
+    const notes = await prisma.note.findMany();
+    res.json(notes);
 })
 
 app.listen(5000, () => {
